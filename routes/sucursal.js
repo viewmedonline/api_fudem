614d09b2835e7bde42afe7eb5b4f7c9d6bb4ce81
Fernando <fernandoescalona92@gmail.com>
2021-10-16 01:26:38 +0200

    revirtiendo modificacion de endpoint

--- routes/sucursal.js
+++ routes/sucursal.js
@@ -5,7 +5,7 @@ let fs = require('fs')
 let jwt = require('jsonwebtoken')
 let model = require('../model/database_schemas.js')
 let mongoose = require('mongoose')
-let request = require('request');
+let http = require('http');
 
 router.post('/sucursal', (request, response) => {
       let currentSucursal = new model.branchOffice(request.body)
